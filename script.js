13abeed4b8bd7c8955f77faf4a0ad83f2859d2c6
adisuyash <116362593+adisuyash@users.noreply.github.com>
2023-07-29 04:50:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let diceOne = Math.floor(Math.random() * 6 + 1);
+let diceOneImage = "/images/dice" + diceOne + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", diceOneImage);
+
+let diceTwo = Math.floor(Math.random() * 6 + 1);
+let diceTwoImage = "/images/dice" + diceTwo + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", diceTwoImage);
+
+if (diceOne === diceTwo) {
+    document.querySelector("h3").innerHTML = "Match Draw";
+} else if (diceOne > diceTwo) {
+    document.querySelector("h3").innerHTML = "Player 1 wins";
+    // Player1 wins ;
+} else {
+    document.querySelector("h3").innerHTML = "Player 2 wins";
+    // Player2 wins ;
+}
+function refreshPage() {
+    window.location.reload();
+}

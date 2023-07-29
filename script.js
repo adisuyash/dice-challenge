8450daaf1fa196411d1a6bb28a98252b780d8143
adisuyash <116362593+adisuyash@users.noreply.github.com>
2023-07-29 06:30:01 +0530

    comments added for dice1,2

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+// For Dice 1
 let diceOne = Math.floor(Math.random() * 6 + 1);
 let diceOneImage = "/images/dice" + diceOne + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", diceOneImage);
 
+// For Dice 2
 let diceTwo = Math.floor(Math.random() * 6 + 1);
 let diceTwoImage = "/images/dice" + diceTwo + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", diceTwoImage);
 
 if (diceOne === diceTwo) {
     document.querySelector("h3").innerHTML = "Match Draw";
-} else if (diceOne > diceTwo) {
+}
+else if (diceOne > diceTwo) {
     document.querySelector("h3").innerHTML = "Player 1 wins";
     // Player1 wins ;
-} else {
+}
+else {
     document.querySelector("h3").innerHTML = "Player 2 wins";
     // Player2 wins ;
 }
